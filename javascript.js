687279f19786e43422cc90d3954712487e0b6609
EryaOvimpakt <170575479+EryaOvimpakt@users.noreply.github.com>
2024-06-16 20:58:19 +0200

    feat: getting human choice
    Added a function that returns "rock", "paper" or "scissors" depending on user input

--- javascript.js
+++ javascript.js
@@ -1,7 +1,16 @@
 function getComputerChoice() {
     let randomNumber = Math.random()
-    if (randomNumber <= 0.33) {return "rock"}
-    else if (randomNumber > 0.66) {return "paper"}
-    else {return "scissors"}
+    if (randomNumber <= 0.33) {return "ROCK"}
+    else if (randomNumber > 0.66) {return "PAPER"}
+    else {return "SCISSORS"}
 }
-console.log(getComputerChoice())
+
+function getHumanChoice() {
+    let humanChoice = prompt("Rock, paper, scissors!").toUpperCase()
+    if (humanChoice == "ROCK"
+        || humanChoice == "PAPER"
+        || humanChoice == "SCISSORS") {return humanChoice }
+    else {return getHumanChoice()
+    }
+}
+console.log(getHumanChoice())
